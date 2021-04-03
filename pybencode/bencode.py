0d829615c07e591e8a8962ec42885c2dfcb1b4ad
cpyberry <cpyberry222@gmail.com>
2021-04-03 12:32:41 +0900

    feat: add a function encode_dictionary

--- pybencode/bencode.py
+++ pybencode/bencode.py
@@ -47,3 +47,24 @@ class Encode:
 			data += add_data
 
 		return b"l" + data + b"e"
+
+	@classmethod
+	def encode_dictionary(cls, dictionary: dict) -> bytes:
+		"""Convert dict type to bencode format
+
+		Args:
+			dictionary (dict): you want to convert to bencode format
+
+		Returns:
+			bytes: bencode format
+
+		Todo:
+			* Create a function that automatically determines types and converts it to bencode format
+		"""
+		data = b""
+		for key, value in dictionary.items():
+			encode_key = cls.encode(key)
+			encode_value = cls.encode(value)
+			data += encode_key + encode_value
+
+		return b"d" + data + b"e"
