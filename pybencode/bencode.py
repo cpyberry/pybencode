551c0089701c5e33eb704f577f61f819c676b087
cpyberry <cpyberry222@gmail.com>
2021-04-03 12:16:55 +0900

    feat: add a function encode_integer

--- pybencode/bencode.py
+++ pybencode/bencode.py
@@ -14,3 +14,16 @@ class Encode:
 		"""
 		data_len = str(len(data)).encode(ENCODING)
 		return data_len + b":" + data
+
+	@staticmethod
+	def encode_integer(integer: int) -> bytes:
+		"""Convert int type to bencode format
+
+		Args:
+			integer (int): you want to convert to bencode format
+
+		Returns:
+			bytes: bencode format
+		"""
+		integer = str(integer).encode(ENCODING)
+		return b"i" + integer + b"e"
