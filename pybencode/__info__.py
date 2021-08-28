904ae994510cee4189677bf39bbcb8a6bb824348
cpyberry <cpyberry222@gmail.com>
2021-08-28 20:29:57 +0900

    refactor: move information in __init__.py to __info__.py
    To make it easier to load with setup.py

---
+++ pybencode/__info__.py
@@ -0,0 +1,6 @@
+__copyright__ = "Copyright 2021 cpyberry"
+__url__ = "https://github.com/cpyberry/pybencode"
+__license__ = "Apache-2.0 License"
+__version__ = "1.0.1"
+__author__ = "cpyberry"
+__author_email__ = "cpyberry222@gmail.com"
